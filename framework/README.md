0d18c4414a40028c7139ef5b82ab30062b93b067
shenzhe <shenzhe163@gmail.com>
2013-10-22 10:48:39 +0800

    add new chat

--- framework/README.md
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/shenzhe/zphp.git ./
-=====================
-
--- inf/default/socket.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('SOCKET_SERVER_HOST', '0.0.0.0');
-define('SOCKET_SERVER_PORT', 8991);
-define('SENDBOX_SERVER_PORT', 843);
-
